5a6b93061e28414785d1d732f2b11e24265ab432
Tim Tochtermann <tim.tochtermann@posteo.de>
2015-05-22 13:07:17 +0200

    add euro currency filter

--- js/main.js
+++ js/main.js
@@ -33,8 +33,25 @@ bebberApp.filter('find', function() {
     });
     return tmp;
   };
-})
+});
+
+bebberApp.filter('euro', function() {
+  return function(price) {
+    return price.formatMoney(2, ',', '.') + ' €'
+  }
+});
 
 bebberApp.factory("accData",function(){
         return {};
 });
+
+Number.prototype.formatMoney = function(c, d, t){
+var n = this, 
+    c = isNaN(c = Math.abs(c)) ? 2 : c, 
+    d = d == undefined ? "." : d, 
+    t = t == undefined ? "," : t, 
+    s = n < 0 ? "-" : "", 
+    i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", 
+    j = (j = i.length) > 3 ? j % 3 : 0;
+   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+ };
